1171999f3ff0b674aa54ecc9550f813116e88610
darod <daniel_arod@protonmail.com>
2022-11-07 11:52:57 -0500

    unidos cambios de jusue y nasalord

--- map.js
+++ map.js
@@ -1,17 +1,13 @@
-var map = L.map('map').setView([4.704230, -74.040566], 12);
+const origin = [4.62027, -74.103551];
+var map = L.map('map').setView(origin, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
-var marker = L.marker([4.603754, -74.079684]).addTo(map);
-
-
-
-
-var marker2 = L.marker([4.624194, -74.076283]).addTo(map);
-var marker3 = L.marker([4.621037, -74.166030]).addTo(map);
+var cr13cl41Marker = L.marker([4.628984, -74.066974]).addTo(map);
+var cr80dg43Marker = L.marker([4.624194, -74.076283]).addTo(map);
+var cl34cr28Marker = L.marker([4.621037, -74.166030]).addTo(map);
 
 var popup = L.popup();
 
@@ -23,8 +19,14 @@ function onMapClick(e) {
         document.getElementById('informacion').innerHTML="<h2>"+e.latlng+"</h2>";
 
     console.log(e)
+    map.setView(e.latlng, 19);
+    document.getElementById("desc").style.display = "block";
 }
+cr13cl41Marker.on('click', onMapClick);
+cr80dg43Marker.on('click', onMapClick);
+cl34cr28Marker.on('click', onMapClick);
 
-marker.on('click', onMapClick);
-marker2.on('click', onMapClick);
-marker3.on('click', onMapClick);
+function onClickBackButton(e) {
+    map.setView(origin, 13);
+    document.getElementById("desc").style.display = "none";
+}
