7186ac4bb4e39680401b4b7385e66284535957a3
darod <daniel_arod@protonmail.com>
2022-11-07 22:34:17 -0500

    texto se restablece al dar clic en botón

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
+var initmsg = "<H2>Da clic a un marcador del mapa</H2>"
 var cr13cl41Marker = L.marker([4.628984, -74.066974]).addTo(map);
 var cr80dg43Marker = L.marker([4.624194, -74.076283]).addTo(map);
 var cl34cr28Marker = L.marker([4.621037, -74.166030]).addTo(map);
@@ -18,11 +18,11 @@ function onMapClick(e) {
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
-        document.getElementById('informacion').innerHTML="<h2>"+e.latlng+"</h2>";
+        document.getElementById('informacion').innerHTML="<h2> Las coordenadas del semaforo son: "+e.latlng+"</h2>";
 
     console.log(e)
     map.setView(e.latlng, 19);
-    document.getElementById("desc").style.display = "block";
+
 }
 
 cr13cl41Marker.on('click', onMapClick);
@@ -31,7 +31,9 @@ cl34cr28Marker.on('click', onMapClick);
 
 function onClickBackButton(e) {
     map.setView(origin, 13);
+    document.getElementById('informacion').innerHTML=initmsg;
     document.getElementById("desc").style.display = "none";
+    
 }
 
 
